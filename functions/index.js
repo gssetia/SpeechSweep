be7373ea5e27ba5692fff1a3809cb0d34ecef524
Gauravdeep Setia <gaurav.singh.setia@gmail.com>
2023-11-07 02:12:44 -0800

    Schedule function sends to every device

--- functions/index.js
+++ functions/index.js
@@ -9,16 +9,52 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
+const {onSchedule} = require("firebase-functions/v2/scheduler");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+
+
+// admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
+
+exports.schedulePushNotification = onSchedule("every day 06:51", async (event) => {
+    const notification = {
+        title: "Sweep!",
+        body: "Here is custom message",
+    }
+
+    admin.database().ref('/users/').once('value', (snapshot) => {
+
+        console.log(snapshot.val());
+
+        snapshot.forEach((childSnapshot) => {
+          const data = childSnapshot.val();
+          const token = data['notification_token'];
+          
+          
+        //   console.log(data);
+          payload = {
+            token: token,
+            notification: notification
+          };
+          admin
+           .messaging()
+           .send(payload)
+           .then(function(response) {
+             console.log("Notification sent successfully to given user:", response);
+           })
+           .catch(function(error) {
+             console.log("Notification sent failed:", error);
+           });
+        });
+    });
+    
+  });
+
 exports.sendPushNotification = functions.database
   .ref("users/{userID}")
   .onCreate(event => {
+
     const token = event._data.notification_token;
     payload = {
        token: token,
@@ -27,13 +63,13 @@ exports.sendPushNotification = functions.database
         body: "thank for installed our app",
       },
     };
-    admin
-      .messaging()
-      .send(payload)
-      .then(function(response) {
-        console.log("Notification sent successfully:", response);
-      })
-      .catch(function(error) {
-        console.log("Notification sent failed:", error);
-      });
+    // admin
+    //   .messaging()
+    //   .send(payload)
+    //   .then(function(response) {
+    //     console.log("Notification sent successfully:", response);
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Notification sent failed:", error);
+    //   });
   });
